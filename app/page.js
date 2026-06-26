2ce4483bed4ff6c8223b4e41a35e9f192d2ed864
Sebastien Ricci <sebastienriccidirector@gmail.com>
2026-06-26 07:01:34 -0700

    Move FAQ below the Contact section (contact now comes first)

--- app/page.js
+++ app/page.js
@@ -36,8 +36,8 @@ export default function Home() {
         <Gallery />
         <KineticBand />
         <Locations />
-        <Faq />
         <Contact />
+        <Faq />
       </main>
       <Footer />
     </>
